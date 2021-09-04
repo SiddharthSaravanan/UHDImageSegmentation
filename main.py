2f7806d2d95c9fe961b8e04bdcc92f73d72ad389
Siddharth Saravanan <69727229+SiddharthSaravanan@users.noreply.github.com>
2021-09-04 08:26:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ parser.add_argument('--network', type=str, default= 'deeplab', choices = ['deepl
 parser.add_argument('--output', type=str, default='./refinement_results/', help='path where refinement results are saved')
 parser.add_argument('--not_sure', type=bool, default=True, help='set to True if you want to include not_sure labels when upscaling/converting network results to images')
 parser.add_argument('--upscale', type=bool, default=True, help='set to True if you need to upscale images back to their original high resolution')
-parser.add_argument('--x', type=int, default=30, help='Number of iterations to perform thinning')
-parser.add_argument('--beta', type=int, default=111, help='Beta for Random Walker')
-parser.add_argument('--prune', type=int, default=11, help='Number of iterations to perform pruning')
+parser.add_argument('--x', type=int, default=31, help='Number of iterations to perform thinning')
+parser.add_argument('--beta', type=int, default=115, help='Beta for Random Walker')
+parser.add_argument('--prune', type=int, default=14, help='Number of iterations to perform pruning')
 parser.add_argument('--evaluate', type=bool, default=True, help='set to True if you want to evaluate the results using gt_images')
 
 args = parser.parse_args()
@@ -25,15 +25,15 @@ args = parser.parse_args()
 
 def main():
 
-	# generate dollar gradient images
-	gen_gradient.gen_grad(args.dataset)
+	#generate dollar gradient images
+	# gen_gradient.gen_grad(args.dataset)
 
-	# upscaling
-	upscale_softmax.gen_probs(args.dataset)
+	#upscaling
+	# upscale_softmax.gen_probs(args.dataset)
 	up.upscale(args.dataset,args.network,args.upscale,args.not_sure)
 
 	#run random walker
-	refine.refinement(args.dataset,args.x,args.prune,args.beta,)
+	refine.refinement(args.dataset,args.x,args.prune,args.beta)
 
 	#evaluate iou
 	if args.evaluate:
