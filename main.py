a52824016cb1efebea37bc208e77989458fc70bc
Siddharth Saravanan <69727229+SiddharthSaravanan@users.noreply.github.com>
2021-09-06 15:43:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ from iou_evaluation import evaluate
 
 
 parser = argparse.ArgumentParser(description='Refine and evaluate')
-parser.add_argument('--dataset', type=str, default='BIG', choices = ['BIG','pascal','custom'], help='dataset to be used')
+parser.add_argument('--dataset', type=str, default='pascal', choices = ['BIG','pascal','custom'], help='dataset to be used')
 parser.add_argument('--network', type=str, default= 'deeplab', choices = ['deeplab','fcn'], help='network used to generate segmentation')
 parser.add_argument('--output', type=str, default='./refinement_results/', help='path where refinement results are saved')
 parser.add_argument('--not_sure', type=bool, default=True, help='set to True if you want to include not_sure labels when upscaling/converting network results to images')
 parser.add_argument('--upscale', type=bool, default=True, help='set to True if you need to upscale images back to their original high resolution')
 parser.add_argument('--x', type=int, default=31, help='Number of iterations to perform thinning')
 parser.add_argument('--beta', type=int, default=115, help='Beta for Random Walker')
-parser.add_argument('--prune', type=int, default=14, help='Number of iterations to perform pruning')
+parser.add_argument('--prune', type=int, default=15, help='Number of iterations to perform pruning')
 parser.add_argument('--evaluate', type=bool, default=True, help='set to True if you want to evaluate the results using gt_images')
 
 args = parser.parse_args()
 
 #------------------------------------------------
+
+params = { 
+'deeplab': {'prob':0.115, 'x':31 , 'beta':115 , 'prune':15},
+'fcn': {'prob':0.115, 'x':31 , 'beta':115 , 'prune':15}
+}
+
+#------------------------------------------------
+
+
 
 def main():
 
@@ -30,14 +39,14 @@ def main():
 
 	#upscaling
 	# upscale_softmax.gen_probs(args.dataset)
-	up.upscale(args.dataset,args.network,args.upscale,args.not_sure)
+	# up.upscale(args.dataset,args.network,args.upscale,args.not_sure)
 
 	#run random walker
 	refine.refinement(args.dataset,args.x,args.prune,args.beta)
 
 	#evaluate iou
 	if args.evaluate:
-		evaluate.eval_iou(args.dataset)
+		evaluate.eval_iou(args.dataset,args.beta)
 
 
 
