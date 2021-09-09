e27cdea38ba019d8cfbe4f834f3c27cb00ba8c7d
Siddharth Saravanan <69727229+SiddharthSaravanan@users.noreply.github.com>
2021-09-09 21:26:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ parser.add_argument('--prune', type=int, default=13, help='Number of iterations 
 parser.add_argument('--evaluate', type=bool, default=True, help='set to True if you want to evaluate the results using gt_images')
 
 args = parser.parse_args()
+
 #------------------------------------------------
 
 def main():
