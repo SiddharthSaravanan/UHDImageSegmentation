18c0a7d2eb3504b26dcdfea966e4f6cd3d1b3376
Siddharth Saravanan <69727229+SiddharthSaravanan@users.noreply.github.com>
2021-10-25 12:28:41 +0530

    Update downsample_512.py

--- downsample_512.py
+++ downsample_512.py
@@ -6,43 +6,16 @@ import random
 import math
 import os,sys
 
-
-# for fname in os.listdir('./Datasets/BIG/val_im/'):
-# 	file = os.path.join('./Datasets/BIG/val_im/',fname)
-# 	print(fname.split('.')[0])
-
-# sys.exit()
 no=0
-# smallest=10000000000000000
-# large_axis = 0
+im_path = 'D:/downsamples/'+str(max_dim)+'/im/'
+gt_path = 'D:/downsamples/'+str(max_dim)+'/gt/'
 
-# for fname in os.listdir('./Datasets/BIG/test_im/'):
-# 	file = os.path.join('./Datasets/BIG/test_im/',fname)
-
-# 	img = cv2.imread(file,cv2.IMREAD_COLOR)
-# 	print(img.shape)
-
-# 	if img.shape[0]>img.shape[1]:
-# 		large_axis = img.shape[0]
-# 	else:
-# 		large_axis = img.shape[1]
-
-# 	if large_axis<smallest:
-# 		smallest = large_axis
-
-# print(smallest)
-# sys.exit(0)
-# for max_dim in [30.0,90.0,150.0,210.0,270.0,330.0,390.0,450.0]:
 for max_dim in [512.0]:
-	os.makedirs('D:/UG/IP SOP/downsamples/'+str(max_dim)+'/im/')
-	os.makedirs('D:/UG/IP SOP/downsamples/'+str(max_dim)+'/gt/')
+	os.makedirs(im_path)
+	os.makedirs(gt_path)
 
 	for fname in os.listdir('./Datasets/BIG/val_im/'):
 		file = os.path.join('./Datasets/BIG/val_im/',fname)
-
-		# file1=open("test.txt","a")
-		# file1.write(fname.split('.')[0]+'\n')
-		# file1.close()
 
 		no+=1
 		img = cv2.imread(file,cv2.IMREAD_COLOR)
@@ -59,16 +32,10 @@ for max_dim in [512.0]:
 
 		final_w = float(w)/scale_factor
 		final_h = float(h)/scale_factor
-		# print(h)
-		# print(w)
-		# max_dim=512
 		
 		scale_factor=0
 		img_resized = cv2.resize(img, dsize = (int(final_w),int(final_h)) , interpolation=cv2.INTER_CUBIC)
-
-
 		img_label = np.zeros(img_resized.shape)
-		# print('./downsamples/'+str(max_dim)+'/im/'+fname.split('.')[0]+'.png')
+
 		cv2.imwrite('./downsamples/'+str(max_dim)+'/im/'+fname.split('.')[0]+'.png',img_resized)
 		cv2.imwrite('./downsamples/'+str(max_dim)+'/gt/'+fname.split('.')[0]+'.png',img_label)
-
